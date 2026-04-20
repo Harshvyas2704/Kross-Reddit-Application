bcf8e0626417260ab9dc52a512dce6ee2aea6ab1
Harshvyas2704 <harshvyasdev@gmail.com>
2026-04-20 20:54:15 +0530

    MMKV and useAuthStore changes

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand';
+import { persist, StateStorage, createJSONStorage } from 'zustand/middleware';
+import { createMMKV } from 'react-native-mmkv';
 
 interface AuthState {
   user: { id: string; email: string } | null;
@@ -14,13 +16,39 @@ interface AuthState {
   updateToken: (newToken: string) => void;
 }
 
-export const useAuthStore = create<AuthState>(set => ({
-  user: null,
-  accessToken: null,
-  refreshToken: null,
-  isLoading: false,
-  setAuth: (user, accessToken, refreshToken) =>
-    set({ user, accessToken, refreshToken }),
-  clearAuth: () => set({ user: null, accessToken: null, refreshToken: null }),
-  updateToken: accessToken => set({ accessToken }),
-}));
+export const storage = createMMKV({
+  id: 'auth-storage',
+});
+
+const zustandStorage: StateStorage = {
+  setItem: (name, value) => {
+    return storage.set(name, value);
+  },
+  getItem: name => {
+    const value = storage.getString(name);
+    return value ?? null;
+  },
+  removeItem: name => {
+    return storage.remove(name);
+  },
+};
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    set => ({
+      user: null,
+      accessToken: null,
+      refreshToken: null,
+      isLoading: false,
+      setAuth: (user, accessToken, refreshToken) =>
+        set({ user, accessToken, refreshToken }),
+      clearAuth: () =>
+        set({ user: null, accessToken: null, refreshToken: null }),
+      updateToken: accessToken => set({ accessToken }),
+    }),
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => zustandStorage),
+    },
+  ),
+);
